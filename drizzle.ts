aa3f6c097827e1d0de970a197573a0efc3f1d3bf
Maciej Budzinski <maciej.budzinski.93@gmail.com>
2025-02-11 08:43:09 +0100

    update drizzle code

--- drizzle.ts
+++ drizzle.ts
@@ -1,9 +1,15 @@
+// It crashes the app:
 import { pgTable, boolean } from 'drizzle-orm/pg-core';
+// It would work:
+// import { pgTable, boolean as dBoolean } from 'drizzle-orm/pg-core';
 
 
 export const ExampleTable = pgTable(
   'example_table',
   {
+    // It crashes the app:
     isCollectable: boolean('is_collectable').notNull().default(true),
+    // It would work
+    //isCollectable: dBoolean('is_collectable').notNull().default(true),
   }
 );
